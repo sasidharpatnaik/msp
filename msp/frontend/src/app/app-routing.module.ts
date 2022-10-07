20bb9c443fb8e3c752548d48bbb4337141c68635
Sasidhar <sasidhar.kuppili@gmail.com>
2022-10-08 03:36:07 +0530

    Initial import

--- msp/frontend/src/app/app-routing.module.ts
+++ msp/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashbordComponent } from './components/dashbord.component';
 import { HomeComponent } from './components/home.component';
+import { LoginComponent } from './components/login.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent}
+  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'dashbord', component:DashbordComponent}
 ];
 
 @NgModule({
--- msp/frontend/src/app/app.module.ts
+++ msp/frontend/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home.component';
+import { LoginComponent } from './components/login.component';
+import { DashbordComponent } from './components/dashbord.component';
+import { HeaderComponent } from './components/header.component';
+import { FooterComponent } from './components/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    DashbordComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
